4922913246138e21d297added7e25ae6539320c7
imanshu11 <anshudeo2000@gmail.com>
2022-02-15 01:15:49 +0530

    Added more propeties details

--- jsm_realestate/components/Property.jsx
+++ jsm_realestate/components/Property.jsx
@@ -4,7 +4,7 @@ import { Box, Flex, Text, Avatar } from '@chakra-ui/react';
 import { FaBed, FaBath} from 'react-icons/fa';
 import { BsGridFill } from 'react-icons/bs';
 import { GoVerified } from 'react-icons/go';
-import milliy from 'millify';
+import milliy, { millify } from 'millify';
 
 import DfaultImage from '../assets/images/house.jpg';
 
@@ -13,6 +13,26 @@ const Property = ({ property: { coverPhoto, price, rentFrequency, rooms, title, 
       <Flex flexWrap="wrap" w="420px" p="5" paddingTop="0" justifyContent="flex-start" cursor="pointer">
           <Box>
               <Image src={coverPhoto ? coverPhoto.url : DefaultImage} width={400} height={260} alt="house" />
+          </Box>
+          <Box w="full">
+              <Flex paddingTop="2" alignItems="center" justifyContent="space-between">
+                  <Flex alignItems="center">
+                      <Box paddingRight="3" color="green.400">
+                          {isVerified && <GoVerified />}
+                      </Box>
+                      <Text fontWeight="bold" fontSize="lg">AED {millify(price)}{rentFrequency && `/${rentFrequency}`}</Text>
+                    </Flex>
+                    <Box>
+                        <Avatar size="sm" src={agency?.logo?.url} />
+                    </Box>
+              </Flex> 
+              <Flex alignItems="center" p="1" justifyContent="space-between" w="250" color="blue.400">
+                  {rooms} <FaBed /> | {baths} <FaBath /> | {millify(area)} sqft <BsGridFill />
+              </Flex>
+              <Text>
+                  {title.length > 30 ? `${title.substring(0,30)}...` : title}
+              </Text>
+
           </Box>
           </Flex>
     </Link>
