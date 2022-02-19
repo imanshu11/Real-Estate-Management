9b9c9a44e70bf33c1135bc56a14ffdb1a430a3c5
imanshu11 <anshudeo2000@gmail.com>
2022-02-20 03:21:41 +0530

    Image Scrollbar Initialized

---
+++ jsm_realestate/components/ImageScrollbar.jsx
@@ -0,0 +1,4 @@
+import { useContext } from 'react';
+import Image from 'next/image';
+import { Box, Icon, Flex } from '@chakra-ui/react';
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
